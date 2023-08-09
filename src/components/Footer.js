354bcb00aae5f417d9ae36aac2caea34be81babb
Roberto Rodríguez <98791278+RodriguezRoberto@users.noreply.github.com>
2023-08-08 20:19:05 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,11 @@ const Footer = () => {
     return (
         <div className='footer'>
             <p>API consumption example</p>
-            <p>Endpoint: https://rickandmortyapi.com/api/location/</p>
             <p>Contact:</p>
+            <p>www.iammrobertorodriguez.com</p>
             <p>iamrobertorodriguez@proton.me</p>
-            <p>https://iamrobertorodriguez.netlify.app</p>
             <p>Copyright © 2022: Roberto Rodríguez, México 55070</p>
-            <p>Hecho con ❤ en Academlo.</p>
+            <p>Hecho con ❤</p>
         </div>
     );
 };
